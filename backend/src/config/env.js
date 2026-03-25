f72fceb9a33466a7f1e1a166e0e065c55a22c75b
kuang-suo <893172768@qq.com>
2026-03-25 14:21:18 +0800

    Update env.js

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -15,7 +15,7 @@ export const config = {
   cors: {
     origin: process.env.FRONTEND_URL || 'http://localhost:3000'
   },
-  port: parseInt(process.env.PORT || '3001', 10),
+  port: parseInt(process.env.PORT || '8080', 10),
   nodeEnv: process.env.NODE_ENV || 'development'
 };
 
